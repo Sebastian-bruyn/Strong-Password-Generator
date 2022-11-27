7254cb976f88eeda15b575b7fc51d60aa2c3c38f
Sebastian-bruyn <sebastian.bruyn@gmail.com>
2022-11-27 18:18:35 +0200

    update

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 import random
 import string
-lengtgOfPassword = 16
+LengthOfPassword = 16
 password = ''
 characters = string.ascii_letters + string.digits + string.punctuation
-for x in range(lengtgOfPassword):
+for x in range(LengthOfPassword):
 
     password = password + random.choice(characters)
 
